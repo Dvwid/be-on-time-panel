c3a2af724cf0562491fa812d97c78d7cd21c7ff4
dhutyra <dhutyra@rekord.com.pl>
2023-08-25 12:01:47 +0200

    BOT-4 - Add homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {AuthService} from "../core/auth/services/auth.service";
+import {UserDto} from "../core/dtos/UserDto";
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
+  currentUser: UserDto;
+
+  #authService = inject(AuthService);
+
+  ngOnInit() {
+    this.currentUser = this.#authService.currentUser$.value;
+  }
 }
