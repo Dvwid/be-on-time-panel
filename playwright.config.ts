b4df5f460a4091a8126139d764f236781f962d60
dhutyra <dhutyra@rekord.com.pl>
2024-01-21 20:55:51 +0100

    Update retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env?.['CI'],
-  retries: process.env?.['CI'] ? 2 : 0,
+  retries: 0,
   workers: 1,
   timeout: 1000 * 60 * 5,
   reporter: 'html',
